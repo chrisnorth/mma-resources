97e9778c1391d1a3a3664b856e36112eca50f1b6
Chris North <chris.north@astro.cf.ac.uk>
2021-12-18 22:40:14 +0000

    move plotting to within class

--- python/GW/plot-locmaps.py
+++ python/GW/plot-locmaps.py
@@ -16,4 +16,5 @@ import mmapy
 
 dets=mmapy.gw.readDetPairs(os.path.join(dataDir,'init_params.json'))
 
-mmapy.gw.plotMaps(dets,plotDir=os.path.join(plotDir,'GW'),dataDir=os.path.join(dataDir,'GW'),plottype='imshow')
+mmapy.gw.plotMaps(dets,plotDir=os.path.join(plotDir,'GW'),dataDir=os.path.join(dataDir,'GW'),verbose=True)
+mmapy.gw.plotMaps(dets,plotDir=os.path.join(plotDir,'GW'),dataDir=os.path.join(dataDir,'GW'),plottype='contour',verbose=True,grid=5)
--- python/mmapy/gw.py
+++ python/mmapy/gw.py
@@ -83,6 +83,174 @@ class DetectorPair(object):
     def vec2dt(self,vec):
         return((np.dot(vec,self.vec))*const.R_earth/const.c)
         
+    def dtData(self,grid=15,csvFile='',units='ms',decimals=2,overwrite=False,verbose=False):
+        print('dtData',verbose)
+        recalc=True
+        if hasattr(self,'dtarr'):
+            dtobj=self.dtarr
+            if overwrite:
+                recalc=True
+                if verbose:print('overwriting dt array for {} with grid {}'.format(self.code,grid))
+            else:
+                try:
+                    if grid!=dtobj['grid']:
+                        recalc=True
+                        print('WARNING: existing grid [] does not match requested grid []. Recalculating'.format(dtobj['grid'],grid))
+                    else:
+                        recalc=False
+                        if verbose:print('using existing dt array for {}'.format(self.code))
+                except:
+                    recalc=True
+                    print('WARNING: unable to compare existing grid [] with requested grid []. Recalculating'.format(dtobj['grid'],grid))
+            dtarr=dtobj['arr']
+            gridsize=[dtobj['grid'],dtobj['grid']]
+        print('recalc',recalc)
+        if recalc:
+            if verbose:print('calculating dt array for {} with grid {}'.format(self.code,grid))
+            gridsize=[grid,grid]
+            
+            nRA=int(360/gridsize[0])
+            nDec=int(180/gridsize[1])
+            gridRAim=np.arange(gridsize[0]/2,360,gridsize[0])
+            gridDecim=np.arange(90-gridsize[1]/2,-90,-gridsize[1])
+            dtarr=np.zeros([nDec,nRA])
+            
+            for r in range(nRA):
+                for d in range(nDec):
+                    # get vector for sky localisation
+                    vec=lb2vec(gridRAim[r],gridDecim[d])
+                    # dt = vec . (d1-d1) * R_earth / c
+                    dt=self.vec2dt(vec)
+                    dtarr[d,r]=dt.to(units).value
+                    
+            if np.max(dtarr)<=15:
+                dt_res=2
+            elif np.max(dtarr)<50:
+                dt_res=5
+            dtobj={'arr':dtarr,'grid':grid,'dt_res':dt_res}
+            self.dtarr=dtobj
+            
+        if csvFile:
+            nRA=int(360/gridsize[0])
+            nDec=int(180/gridsize[1])
+            dtcsv=np.zeros([nDec,nRA])
+            for r in range(nRA):
+                for d in range(nDec):
+                    dtcsv[d,r]=np.around(dtarr[d,r],decimals=decimals)
+            df=pd.DataFrame(dtcsv)
+            if verbose:print('saving dt array for {} to {}'.format(self.code,csvFile))
+            df.to_csv(csvFile,header=False,index=False)
+        
+        return(dtobj)
+        
+    def plotdtMap(self,grid=15,pngFile='',fignum=None,units='ms',colormap='jet',verbose=False):
+        if np.isscalar(grid):
+            gridsize=[grid,grid]
+        else:
+            gridsize=[grid[0],grid[1]]
+        
+        nRA=int(360/gridsize[0])
+        nDec=int(180/gridsize[1])
+        dtarr=self.dtData(grid=grid,units=units,verbose=verbose)
+        
+        labgridRA=np.arange(0,360+gridsize[0],gridsize[0])
+        labgridDec=np.arange(-90,90+gridsize[1],gridsize[1])
+            
+        fig=plt.figure(fignum)
+        plt.clf()
+        
+        dcol=dtarr['dt_res']
+        # set colour levels
+        cmin=np.floor(np.min(dtarr['arr'])/dcol)*dcol
+        cmax=np.ceil(np.max(dtarr['arr'])/dcol)*dcol
+        clev=2*cmax/dcol
+        cmap=cm.get_cmap(colormap,clev)
+        cticks=np.linspace(cmin,cmax,int(clev+1))
+        
+        dRAgrid=nRA/24
+        dDecgrid=nDec/12
+        plt.imshow(dtarr['arr'],aspect='equal',cmap=cmap,vmin=cmin,vmax=cmax)
+        ax=plt.gca()
+        ax.set_aspect('equal')
+        # add ticks and labels
+        ax.set_xticks(np.arange(0,nRA,dRAgrid)-0.5,['']*24)
+        ax.set_xticks(np.arange(np.max([0,(dRAgrid-1)/2]),nRA,dRAgrid),string.ascii_uppercase[:24],minor=True)
+        ax.set_yticks(np.arange(0,nDec,dDecgrid)-0.5,['']*12)
+        ax.set_yticks(np.arange(np.max([0,(dDecgrid-1)/2]),nDec,dDecgrid),range(12),minor=True)
+        ax.tick_params(axis='both',which='minor',length=0)
+        
+        ax.set_aspect('equal')
+        plt.title('Time Difference: {}'.format(self.name))
+        ax.xaxis.set_ticks_position('bottom')
+        ax.yaxis.set_ticks_position('left')
+        ax.grid(axis='both',which='major',alpha=1)
+        plt.colorbar(location='bottom',label='Time difference ({})'.format(units),ticks=cticks)
+        # pngFile=os.path.join(plotDir,'dt_contour_{}.png'.format(dd))
+        if pngFile:
+            if verbose:print('saving dt map for {} to {}'.format(self.code,pngFile))
+            plt.savefig(pngFile)
+
+        return(fig)
+    
+    def plotdtMapContour(self,grid=15,pngFile='',fignum=None,units='ms',colormap='jet',verbose=False):
+        gridsize=[grid,grid]
+        
+        # use contour plot
+        # set grid for contour plot - on gridlines, including final line
+        nRA=int(360/gridsize[0])+1
+        nDec=int(180/gridsize[1])+1
+        gridRA=np.arange(0,360+gridsize[0],gridsize[0])
+        gridDec=np.arange(-90,90+gridsize[1],gridsize[1])
+        
+        dtarr=np.zeros([nDec,nRA])
+        for r in range(nRA):
+            for d in range(nDec):
+                # get vector for sky localisation
+                vec=lb2vec(gridRA[r],gridDec[d])
+                # dt = vec . (d1-d1) * R_earth / c
+                dt=self.vec2dt(vec)
+                dtarr[d,r]=dt.to(units).value    
+            
+        fig=plt.figure(fignum)
+        plt.clf()
+        
+        # set colour levels
+        if np.max(dtarr)<=15:
+            dcol=2
+        elif np.max(dtarr)<50:
+            dcol=5
+        cmin=np.floor(np.min(dtarr)/dcol)*dcol
+        cmax=np.ceil(np.max(dtarr)/dcol)*dcol
+        clev=2*cmax/dcol
+        cmap=cm.get_cmap(colormap,clev)
+        cticks=np.linspace(cmin,cmax,int(clev+1))
+        
+        labgridsize=[15,15]
+        labgridRA=np.arange(0,360+labgridsize[0],labgridsize[0])
+        labgridDec=np.arange(-90,90+labgridsize[1],labgridsize[1])
+        
+        plt.contourf(gridRA,gridDec,dtarr,cmap=cmap,vmin=cmin,vmax=cmax,levels=cticks)
+        ax=plt.gca()
+        # add ticks and labels
+        ax.set_xticks(labgridRA,['']*len(labgridRA))
+        ax.set_xticks(labgridRA[:-1]+labgridsize[0]/2,string.ascii_uppercase[:len(labgridRA)-1],minor=True)
+        ax.set_yticks(labgridDec,['']*len(labgridDec))
+        ax.set_yticks(labgridDec[:-1]+labgridsize[1]/2,range(len(labgridDec)-1),minor=True)
+        ax.tick_params(axis='both',which='minor',length=0)
+    
+        ax.set_aspect('equal')
+        plt.title('Time Difference: {}'.format(self.name))
+        ax.xaxis.set_ticks_position('bottom')
+        ax.yaxis.set_ticks_position('left')
+        ax.grid(axis='both',which='major',alpha=1)
+        plt.colorbar(location='bottom',label='Time difference ({})'.format(units),ticks=cticks)
+        # pngFile=os.path.join(plotDir,'dt_contour_{}.png'.format(dd))
+        if pngFile:
+            if verbose:print('saving dt map for {} to {}'.format(self.code,pngFile))
+            plt.savefig(pngFile)
+
+        return(fig)
+        
 def readDetectors(fileIn):
     if isinstance(fileIn,str):
         dataIn=json.load(open(fileIn))
@@ -112,125 +280,23 @@ def readDetPairs(fileIn):
                 pairs[pair.code]=pair
     return(pairs)
 
-def dtData(detpair,grid=15,csvFile='',units='ms',decimals=2):
-    if np.isscalar(grid):
-        gridsize=[grid,grid]
-    else:
-        gridsize=[grid[0],grid[1]]
-    
-    nRA=int(360/gridsize[0])
-    nDec=int(180/gridsize[1])
-    gridRAim=np.arange(gridsize[0]/2,360,gridsize[0])
-    gridDecim=np.arange(90-gridsize[1]/2,-90,-gridsize[1])
-    dtcsv=np.zeros([nDec,nRA])
-    for r in range(nRA):
-        for d in range(nDec):
-            # get vector for sky localisation
-            vec=lb2vec(gridRAim[r],gridDecim[d])
-            # dt = vec . (d1-d1) * R_earth / c
-            dt=detpair.vec2dt(vec)
-            dtcsv[d,r]=np.around(dt.to(units).value,decimals=decimals)
-    df=pd.DataFrame(dtcsv)
-    if csvFile:
-        df.to_csv(csvFile,header=False,index=False)
-
-    return(df)
-
-def dtMap(detpair,grid=15,pngFile='',plottype='imshow',fignum=None,units='ms',colormap='jet'):
+def plotMaps(detpairs,grid=15,plotDir='',dataDir='',
+            fignum=None,units='ms',colormap='jet',plottype='imshow',verbose=False,pngPrefix='',csvPrefix=''):
     allowedtypes=['contour','imshow']
     assert plottype in allowedtypes,'ERROR: INVALID PLOTTYPE [{}]'.format(plottype)
-    if np.isscalar(grid):
-        gridsize=[grid,grid]
-    else:
-        gridsize=[grid[0],grid[1]]
-    
-    if plottype=="contour":
-        # use contour plot
-        # set grid for contour plot - on gridlines, including final line
-        nRA=int(360/gridsize[0])+1
-        nDec=int(180/gridsize[1])+1
-        gridRA=np.arange(0,360+gridsize[0],gridsize[0])
-        gridDec=np.arange(90,-90-gridsize[1],-gridsize[1])
-    else:
-        # use imshow
-        nRA=int(360/gridsize[0])
-        nDec=int(180/gridsize[1])
-        gridRA=np.arange(gridsize[0]/2,360,gridsize[0])
-        gridDec=np.arange(90-gridsize[1]/2,-90,-gridsize[1])
         
-        
-    dtarr=np.zeros([nDec,nRA])
-    for r in range(nRA):
-        for d in range(nDec):
-            # get vector for sky localisation
-            vec=lb2vec(gridRA[r],gridDec[d])
-            # dt = vec . (d1-d1) * R_earth / c
-            dt=detpair.vec2dt(vec)
-            dtarr[d,r]=dt.to(units).value
-                
-                
-    labgridsize=[15,15]
-    labgridRA=np.arange(0,360+labgridsize[0],labgridsize[0])
-    labgridDec=np.arange(-90,90+labgridsize[1],labgridsize[1])
-        
-        
-    fig=plt.figure(fignum)
-    plt.clf()
-    
-    # set colour levels
-    if np.max(dtarr)<=15:
-        dcol=2
-    elif np.max(dtarr)<50:
-        dcol=5
-    cmin=np.floor(np.min(dtarr)/dcol)*dcol
-    cmax=np.ceil(np.max(dtarr)/dcol)*dcol
-    clev=2*cmax/dcol
-    cmap=cm.get_cmap(colormap,clev)
-    cticks=np.linspace(cmin,cmax,int(clev+1))
-    
-    if plottype=="contour":
-        plt.contourf(gridRA,gridDec,dtarr,cmap=cmap,vmin=cmin,vmax=cmax,levels=cticks)
-        ax=plt.gca()
-        # add ticks and labels
-        ax.set_xticks(labgridRA,['']*len(labgridRA))
-        ax.set_xticks(labgridRA[:-1]+labgridsize[0]/2,string.ascii_uppercase[:len(labgridRA)-1],minor=True)
-        ax.set_yticks(labgridDec,['']*len(labgridDec))
-        ax.set_yticks(labgridDec[:-1]+labgridsize[1]/2,range(len(labgridDec)-1),minor=True)
-        ax.tick_params(axis='both',which='minor',length=0)
-        
-    else:
-        # use imshow
-        dRAgrid=nRA/24
-        dDecgrid=nDec/12
-        plt.imshow(dtarr,aspect='equal',cmap=cmap,vmin=cmin,vmax=cmax)
-        ax=plt.gca()
-        ax.set_aspect('equal')
-        # add ticks and labels
-        ax.set_xticks(np.arange(0,nRA,dRAgrid)-0.5,['']*24)
-        ax.set_xticks(np.arange(np.max([0,(dRAgrid-1)/2]),nRA,dRAgrid),string.ascii_uppercase[:24],minor=True)
-        ax.set_yticks(np.arange(0,nDec,dDecgrid)-0.5,['']*12)
-        ax.set_yticks(np.arange(np.max([0,(dDecgrid-1)/2]),nDec,dDecgrid),range(12),minor=True)
-        ax.tick_params(axis='both',which='minor',length=0)
-    
-    ax.set_aspect('equal')
-    plt.title('Time Difference: {}'.format(detpair.name))
-    ax.xaxis.set_ticks_position('bottom')
-    ax.yaxis.set_ticks_position('left')
-    ax.grid(axis='both',which='major',alpha=1)
-    plt.colorbar(location='bottom',label='Time difference ({})'.format(units),ticks=cticks)
-    # pngFile=os.path.join(plotDir,'dt_contour_{}.png'.format(dd))
-    if pngFile: plt.savefig(pngFile)
-
-    return(fig)
-    
-def plotMaps(detpairs,grid=15,plotDir='',dataDir='',plottype='imshow',
-            fignum=None,units='ms',colormap='jet'):
     p=0
     for dd in detpairs:
-        print('plotting dt map for {}'.format(detpairs[dd].name))
+        if verbose:print('plotting dt map for {} [type={}, grid={}]'.format(detpairs[dd].code,plottype,grid))
         p=p+1
-        pngFile=os.path.join(plotDir,'dt_{}.png'.format(detpairs[dd].code))
-        csvFile=os.path.join(dataDir,'dt_{}.csv'.format(detpairs[dd].code))
-        dtData(detpairs[dd],csvFile=csvFile)
-        dtMap(detpairs[dd],pngFile=pngFile,fignum=p,plottype=plottype,units=units,colormap=colormap)
+        csvFile=os.path.join(dataDir,'{}dt_{}.csv'.format(csvPrefix,detpairs[dd].code))
+        if plottype=='contour':
+            pngFile=os.path.join(plotDir,'{}dt_{}_contour.png'.format(pngPrefix,detpairs[dd].code))
+            detpairs[dd].plotdtMapContour(pngFile=pngFile,fignum=p,units=units,colormap=colormap,verbose=verbose,grid=grid)
+        else:
+            pngFile=os.path.join(plotDir,'{}dt_{}.png'.format(pngPrefix,detpairs[dd].code))
+            detpairs[dd].dtData(csvFile=csvFile,verbose=verbose)
+            detpairs[dd].plotdtMap(pngFile=pngFile,fignum=p,units=units,colormap=colormap,verbose=verbose,grid=grid)
     plt.show()
+    
+
