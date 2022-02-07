0a3c99654adb1a535207f3cad2911542dcce2ae8
Chris North <chris.north@astro.cf.ac.uk>
2022-02-07 00:49:21 +0000

    removed unused code

--- python/mmapy/mma_mod.py
+++ python/mmapy/mma_mod.py
@@ -5,63 +5,6 @@ from . import gw,xray
 from . import utils as ut
 # from . import xray
 
-def d2r(d):
-    # convert degree to radians
-    return(d/180.*np.pi)
-
-def lonlat2vec(loc):
-    """
-    Convert lon,lat to unit vector
-    Input: [list] 2-element list containing [longitude,latitude] (in degrees)
-    Output: [lest] unit vector
-    """
-    vec=np.array([np.cos(d2r(loc[0]))*np.cos(d2r(loc[1])),
-        np.sin(d2r(loc[0]))*np.cos(d2r(loc[1])),
-        np.sin(d2r(loc[1]))])
-    return vec
-
-# def radec2grid(loc):
-#     """
-#     Set location of event, centred on 15deg grid squares
-#     Inputs: None
-#     Output: [float,float]: [lon,lat]
-#     """
-#     grid=15.
-#     gridlon=np.floor(loc[0]/grid)*grid + grid/2
-#     gridlat=np.floor(loc[1]/grid)*grid + grid/2
-#     return [gridlon,gridlat]
-
-class Location(object):
-    def __init__(self,loc):
-        """
-        Set location of event, centred on 15deg grid squares
-        Inputs [float,float]: [longitude, latitude] (degrees)
-        Attributes added: [[float,float],[int,int],string]: [[snaplon,snaplat],[cell x, cell y],cellname]
-            * loc [float,float]: array of [lon,lat] (degrees)
-            * grid [float]: grid size (degrees)
-            * cellloc [float,float]: [lat,lon] of cell centre (degrees)
-            * cellxy [float,float]: [x,y] coordinates of cell
-            * cellname: name of cell
-        Outputs: None
-        """
-        assert len(loc)==2, 'ERROR: non-valid location: {}'.format(loc)
-        assert (loc[1]<=90 and loc[1]>=-90),'ERROR: latitude not in range [-90,90]: {}'.format(loc[1])
-        assert (loc[0]<=360 and loc[0]>=-0),'ERROR: longitude not in range [0,360]: {}'.format(loc[0])
-        self.loc=loc
-        self.grid=15.
-        gridlon=np.floor(self.loc[0]/self.grid)*self.grid + self.grid/2
-        gridlat=np.floor(self.loc[1]/self.grid)*self.grid + self.grid/2
-        self.cellloc=[gridlon,gridlat]
-        self.cellxy=[int(self.cellloc[0]/15),int((90-self.cellloc[1])/15)]
-        
-        # get cell names
-        raStr=string.ascii_uppercase[:24]
-        decStr=[str(x) for x in np.arange(12) + 1]
-        decStr.reverse()
-        self.cellname=raStr[int(self.cellloc[0]/15)]+decStr[int((90-self.cellloc[1])/15)]
-        return
-
-    
 class Event(object):
     def __init__(self,paramin,detectors={}):
         """
