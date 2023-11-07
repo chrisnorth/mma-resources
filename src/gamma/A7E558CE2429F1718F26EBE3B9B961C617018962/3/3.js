33c081ad0612ff3112c16102d2c165fa80da635a
slowe <299787+slowe@users.noreply.github.com>
2023-11-07 23:19:11 +0000

    Update gamma task 3

--- src/gamma/A7E558CE2429F1718F26EBE3B9B961C617018962/3/3.js
+++ src/gamma/A7E558CE2429F1718F26EBE3B9B961C617018962/3/3.js
@@ -46,13 +46,11 @@ function Step(data,opt){
 		});
 
 		var rangeSliderValueElement = document.getElementById('slider-range-value');
-/*
+
 		el.slider.noUiSlider.on('update', function (values, handle) {
 			rangeSliderValueElement.innerHTML = values[handle]+'&deg;';
 			brightness.updateValues();
 		});
-
-*/
 
 		this.updateValues();
 
@@ -82,72 +80,64 @@ function BrightnessIndicator(el,opt){
 		return this;
 	}
 
+	var E_iso_measured = 4.17e39;
+
 	var ev = opt.event;
-	var dist = opt.dist;
+	var dist = opt.dist;	// An array of DOM elements
 	var _obj = this;
 
-	this.getE = function(theta,dist){
-		return Math.pow(10,(9.2 * Math.exp(-0.458*theta) + 37.5));
-		return Math.pow(10,(9.2 * Math.exp(-0.458*theta) + 37.5))/Math.pow((dist/ev.distance),2);
-	};
+	function getE(theta,dist){ return Math.pow(10,(9.2 * Math.exp(-0.458*theta) + 37.5)); };
 
+	// Add brightness indicator elements to DOM
 	var reference = document.createElement('div');
-	reference.classList.add('reference','ga');
-	el.appendChild(reference);
-
+	reference.classList.add('reference','gw');
+	el.querySelector('.inner').appendChild(reference);
 	var indicator = document.createElement('div');
 	indicator.classList.add('indicator','ga-highlight');
-	el.appendChild(indicator);
+	el.querySelector('.inner').appendChild(indicator);
 
+	// Add change events to the distance values
 	dist[0].addEventListener('change',function(e){ _obj.updateValues(); });
 	dist[1].addEventListener('change',function(e){ _obj.updateValues(); });
 
-
-
-	E = Math.log10(4.17e39);
-	d1 = parseFloat(dist[0].value)||0;
-	d2 = parseFloat(dist[1].value)||d1;
-	E_a = this.getE(90,d1);
-	E_b = this.getE(0,d2);
-	l = Math.floor(Math.log10(E_a));
-	r = Math.ceil(Math.log10(E_b));
-
+	var E = Math.log10(E_iso_measured);
+	var E_l = getE(90);
+	var E_r = getE(0);
+	var l = Math.floor(Math.log10(E_l));
+	var r = Math.ceil(Math.log10(E_r));
+	
+	// Update indicator position
+	var Ipc = (100*(E-l)/(r-l));
+	indicator.setAttribute('style','left:'+Ipc.toFixed(2)+'%;');
+	indicator.setAttribute('title','{{ site.translations.main.observatory.gamma.step3.measured }}'.replace(/\{\{ ?value ?\}\}/,E_iso_measured));
 
 	// Update the values
 	this.updateValues = function(){
 
-		d1 = parseFloat(dist[0].value)||0;
-		d2 = parseFloat(dist[1].value)||d1;
-		inc = parseFloat(opt.inclination.noUiSlider.get());
+		var dmin = Math.max(0.1,parseFloat(dist[0].value)||0);
+		var dmax = parseFloat(dist[1].value)||dmin;
+		var inc = parseFloat(opt.inclination.noUiSlider.get());
+		var E_max = getE(inc)/Math.pow((dmin/ev.distance),2);
+		var E_min = getE(inc)/Math.pow((dmax/ev.distance),2);
 
-		E_a = this.getE(inc,d1);
-		E_b = this.getE(inc,d2);
-		l = Math.floor(Math.log10(E_a));
-		r = Math.ceil(Math.log10(E_b));
-		console.log('updateValues',d1,d2,l,r);
-
-		indicator.setAttribute('style','left:'+E.toFixed(2)+'%;');
-
-		reference.setAttribute('style','left:'+l+'%;right:'+r+'%')
+		// Set the left and right edges of the reference area
+		reference.setAttribute('style','left:'+getPC(E_min)+'%;right:'+(100-getPC(E_max))+'%')
 
 		return this;
 	};
 
-
-	this.getPC = function(E){
-		var v = Math.log10(E);
-		var pc = ((v-l)/(r-l))*100;
-		return pc;
+	function getPC(v){
+		return ((Math.log10(v)-l)/(r-l))*100;
 	};
 
-	if(E_a==Infinity || E_b==Infinity){
-		console.error('Range is infinite',E_a,E_b);
+	if(E_l==Infinity || E_r==Infinity){
+		console.error('Range is infinite',E_l,E_r);
 		return this;
 	}
 
 	// Add labels
 	for(i = Math.floor(l); i <= Math.ceil(r); i++){
-		x = this.getPC(Math.pow(10,i));
+		x = getPC(Math.pow(10,i));
 		lbl = document.createElement('div');
 		lbl.classList.add('axis-label');
 		lbl.innerHTML = '<div><div class="tick"></div><span class="label">10<sup>'+i+'</sup></span></div>';
